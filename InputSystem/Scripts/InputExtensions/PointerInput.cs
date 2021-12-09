79a49d73de0eac5fd7a1be93e180b4486c0c7923
ViJ <propellerheadvij@gmail.com>
2021-12-10 00:42:35 +0300

    Added composite binding

---
+++ InputSystem/Scripts/InputExtensions/PointerInput.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace ViJTools
+{
+    /// <summary>
+    /// Simple object to contain information for drag inputs.
+    /// </summary>
+    public struct PointerInput
+    {
+        public bool Contact;
+
+        /// <summary>
+        /// ID of input type.
+        /// </summary>
+        public int InputId;
+
+        /// <summary>
+        /// Position of input device
+        /// </summary>
+        public Vector2 Position;
+
+        /// <summary>
+        /// Orientation of input pen.
+        /// </summary>
+        public Vector2? Tilt;
+
+        /// <summary>
+        /// Pressure of input device.
+        /// </summary>
+        public float? Pressure;
+
+        /// <summary>
+        /// Radius of input device.
+        /// </summary>
+        public Vector2? Radius;
+
+        /// <summary>
+        /// Twist of input device.
+        /// </summary>
+        public float? Twist;
+    }
+}
---
+++ InputSystem/Scripts/InputExtensions/PointerInputComposite.cs
@@ -0,0 +1,63 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+using UnityEngine.InputSystem;
+using UnityEngine.InputSystem.Layouts;
+using UnityEngine.InputSystem.Utilities;
+using ViJTools;
+
+// What we do in PointerInputManager is to simply create a separate action for each input we need for PointerInput.
+// This here shows a possible alternative that sources all inputs as a single value using a composite. Has pros
+// and cons. Biggest pro is that all the controls actuate together and deliver one input value.
+//
+// NOTE: In PointerControls, we are binding mouse and pen separately from touch. If we didn't care about multitouch,
+//       we wouldn't have to to that but could rather just bind `<Pointer>/position` etc. However, to source each touch
+//       as its own separate PointerInput source, we need to have multiple PointerInputComposites.
+#if UNITY_EDITOR
+[InitializeOnLoad]
+#endif
+public class PointerInputComposite : InputBindingComposite<PointerInput>
+{
+
+    [InputControl(layout = "Button")] public int Contact;
+    [InputControl(layout = "Vector2")] public int Position;
+    [InputControl(layout = "Vector2")] public int Tilt;
+    [InputControl(layout = "Vector2")] public int Radius;
+    [InputControl(layout = "Axis")] public int Pressure;
+    [InputControl(layout = "Axis")] public int Twist;
+    [InputControl(layout = "Integer")] public int InputId;
+
+    public override PointerInput ReadValue(ref InputBindingCompositeContext context)
+    {
+        //Primary data
+        var contact = context.ReadValueAsButton(Contact);
+        var pointerId = context.ReadValue<int>(InputId);
+        var position = context.ReadValue<Vector2, Vector2MagnitudeComparer>(Position);
+
+        //Secondary data
+        var pressure = context.ReadValue<float>(Pressure);
+        var radius = context.ReadValue<Vector2, Vector2MagnitudeComparer>(Radius);
+        var tilt = context.ReadValue<Vector2, Vector2MagnitudeComparer>(Tilt);
+        var twist = context.ReadValue<float>(Twist);
+
+        return new PointerInput
+        {
+            Contact = contact,
+            InputId = pointerId,
+            Position = position,
+
+            Tilt = tilt != default ? tilt : (Vector2?)null,
+            Pressure = pressure > 0 ? pressure : (float?)null,
+            Radius = radius.sqrMagnitude > 0 ? radius : (Vector2?)null,
+            Twist = twist > 0 ? twist : (float?)null,
+        };
+    }
+
+#if UNITY_EDITOR
+    static PointerInputComposite() => Register();
+#endif
+
+    [RuntimeInitializeOnLoadMethod(RuntimeInitializeLoadType.BeforeSceneLoad)]
+    private static void Register() => InputSystem.RegisterBindingComposite<PointerInputComposite>();
+}
