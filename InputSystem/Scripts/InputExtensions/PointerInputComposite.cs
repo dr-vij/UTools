b80785feebcbaaa0d220aabc9be9c37b5dd379c4
ViJ <propellerheadvij@gmail.com>
2021-12-13 00:27:23 +0300

    10 finger basic multitouch + mouse + pen. Interaction object are completely broken for now.

--- InputSystem/Scripts/InputExtensions/PointerInputComposite.cs
+++ InputSystem/Scripts/InputExtensions/PointerInputComposite.cs
@@ -2,62 +2,80 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEditor;
 using UnityEngine;
+using UnityEngine.EventSystems;
 using UnityEngine.InputSystem;
 using UnityEngine.InputSystem.Layouts;
 using UnityEngine.InputSystem.Utilities;
 using ViJTools;
 
-// What we do in PointerInputManager is to simply create a separate action for each input we need for PointerInput.
-// This here shows a possible alternative that sources all inputs as a single value using a composite. Has pros
-// and cons. Biggest pro is that all the controls actuate together and deliver one input value.
-//
-// NOTE: In PointerControls, we are binding mouse and pen separately from touch. If we didn't care about multitouch,
-//       we wouldn't have to to that but could rather just bind `<Pointer>/position` etc. However, to source each touch
-//       as its own separate PointerInput source, we need to have multiple PointerInputComposites.
-#if UNITY_EDITOR
-[InitializeOnLoad]
-#endif
-public class PointerInputComposite : InputBindingComposite<PointerInput>
+
+namespace ViJTools
 {
-
-    [InputControl(layout = "Button")] public int Contact;
-    [InputControl(layout = "Vector2")] public int Position;
-    [InputControl(layout = "Vector2")] public int Tilt;
-    [InputControl(layout = "Vector2")] public int Radius;
-    [InputControl(layout = "Axis")] public int Pressure;
-    [InputControl(layout = "Axis")] public int Twist;
-    [InputControl(layout = "Integer")] public int InputId;
-
-    public override PointerInput ReadValue(ref InputBindingCompositeContext context)
+    public static class Helpers
     {
-        //Primary data
-        var contact = context.ReadValueAsButton(Contact);
-        var pointerId = context.ReadValue<int>(InputId);
-        var position = context.ReadValue<Vector2, Vector2MagnitudeComparer>(Position);
-
-        //Secondary data
-        var pressure = context.ReadValue<float>(Pressure);
-        var radius = context.ReadValue<Vector2, Vector2MagnitudeComparer>(Radius);
-        var tilt = context.ReadValue<Vector2, Vector2MagnitudeComparer>(Tilt);
-        var twist = context.ReadValue<float>(Twist);
-
-        return new PointerInput
-        {
-            Contact = contact,
-            InputId = pointerId,
-            Position = position,
-
-            Tilt = tilt != default ? tilt : (Vector2?)null,
-            Pressure = pressure > 0 ? pressure : (float?)null,
-            Radius = radius.sqrMagnitude > 0 ? radius : (Vector2?)null,
-            Twist = twist > 0 ? twist : (float?)null,
-        };
+        public const int LeftMouseInputId = PointerInputModule.kMouseLeftId;
+        public const int RightMouseInputId = PointerInputModule.kMouseRightId;
+        public const int MiddleMouseInputId = PointerInputModule.kMouseMiddleId;
+        public const int PenInputId = int.MinValue;
     }
 
+
+    // What we do in PointerInputManager is to simply create a separate action for each input we need for PointerInput.
+    // This here shows a possible alternative that sources all inputs as a single value using a composite. Has pros
+    // and cons. Biggest pro is that all the controls actuate together and deliver one input value.
+    //
+    // NOTE: In PointerControls, we are binding mouse and pen separately from touch. If we didn't care about multitouch,
+    //       we wouldn't have to to that but could rather just bind `<Pointer>/position` etc. However, to source each touch
+    //       as its own separate PointerInput source, we need to have multiple PointerInputComposites.
 #if UNITY_EDITOR
-    static PointerInputComposite() => Register();
+    [InitializeOnLoad]
+#endif
+    public class PointerInputComposite : InputBindingComposite<PointerInput>
+    {
+
+        [InputControl(layout = "Button")] public int Contact;
+        [InputControl(layout = "Vector2")] public int Position;
+        [InputControl(layout = "Vector2")] public int Tilt;
+        [InputControl(layout = "Vector2")] public int Radius;
+        [InputControl(layout = "Axis")] public int Pressure;
+        [InputControl(layout = "Axis")] public int Twist;
+        [InputControl(layout = "Integer")] public int InputId;
+
+        public override PointerInput ReadValue(ref InputBindingCompositeContext context)
+        {
+            //foreach(var control in  context.controls)
+            //    Debug.Log(control.control.device.GetType());
+
+            //Primary data
+            var contact = context.ReadValueAsButton(Contact);
+            var pointerId = context.ReadValue<int>(InputId);
+            var position = context.ReadValue<Vector2, Vector2MagnitudeComparer>(Position);
+
+            //Secondary data
+            var pressure = context.ReadValue<float>(Pressure);
+            var radius = context.ReadValue<Vector2, Vector2MagnitudeComparer>(Radius);
+            var tilt = context.ReadValue<Vector2, Vector2MagnitudeComparer>(Tilt);
+            var twist = context.ReadValue<float>(Twist);
+
+            return new PointerInput
+            {
+                Contact = contact,
+                InputId = pointerId,
+                Position = position,
+
+                Tilt = tilt != default ? tilt : (Vector2?)null,
+                Pressure = pressure > 0 ? pressure : (float?)null,
+                Radius = radius.sqrMagnitude > 0 ? radius : (Vector2?)null,
+                Twist = twist > 0 ? twist : (float?)null,
+            };
+        }
+
+#if UNITY_EDITOR
+        static PointerInputComposite() => Register();
 #endif
 
-    [RuntimeInitializeOnLoadMethod(RuntimeInitializeLoadType.BeforeSceneLoad)]
-    private static void Register() => InputSystem.RegisterBindingComposite<PointerInputComposite>();
+        [RuntimeInitializeOnLoadMethod(RuntimeInitializeLoadType.BeforeSceneLoad)]
+        private static void Register() => InputSystem.RegisterBindingComposite<PointerInputComposite>();
+    }
+
 }
--- InputSystem/Scripts/InputManager.cs
+++ InputSystem/Scripts/InputManager.cs
@@ -47,14 +47,173 @@ namespace ViJTools
         {
             mActions = new Actions();
             mActions.GestureActions.Enable();
-            mActions.GestureActions.PointerStart.performed += OnPointerPerformed;
-            mActions.GestureActions.PointerStart.canceled += OnPointerCanceled;
-            mActions.GestureActions.PointerMove.performed += OnPointerMove;
+
+            mActions.GestureActions.Pointer.performed += OnPointerEvent;
+            mActions.GestureActions.Pointer.canceled += OnPointerEvent;
         }
 
-        private void Awake()
+        private InputDevice mActiveDevice;
+        private HashSet<int> mActiveTouches = new HashSet<int>();
+        private bool mIsMouseOrPenInputStarted;
+        private Dictionary<int, InteractionPointerData> mActivePointers = new Dictionary<int, InteractionPointerData>();
+
+        public class InteractionPointerData
         {
-            TextDebugger.Instance.Log(Screen.dpi);
+            public int ID { get; private set; }
+
+            public Vector2 PointerStartPosition { get; private set; }
+            public Vector2 PointerPosition { get; private set; }
+            public Vector2 PointerPrevPosition { get; private set; }
+
+            public bool IsPrimary { get; private set; }
+
+            public InteractionPointerData(Vector2 initialPosition, int id, bool isPrimary)
+            {
+                PointerStartPosition = initialPosition;
+                PointerPosition = initialPosition;
+                PointerPrevPosition = initialPosition;
+                IsPrimary = isPrimary;
+                ID = id;
+            }
+
+            public void UpdatePosition(Vector2 newPosition)
+            {
+                PointerPrevPosition = PointerPosition;
+                PointerPosition = newPosition;
+            }
+        }
+
+        private void OnPointerEvent(InputAction.CallbackContext context)
+        {
+            //WE DO NOT ALLOW TO MIX DIFFERENT DEVICES IN THIS COMBINATION OF GESTURES
+            if (CanBeHandled(context))
+            {
+                var data = context.ReadValue<PointerInput>();
+                if (context.control.device is Touchscreen)
+                {
+                    //Check if new contact
+                    if (data.Contact)
+                    {
+                        if (mActiveTouches.Contains(data.InputId))
+                        {
+                            //POINTER DRAG
+                            OnRawPointerDrag(data);
+                        }
+                        else
+                        {
+                            mActiveTouches.Add(data.InputId);
+                            mActiveDevice = context.control.device;
+                            //POINTER DOWN
+                            OnRawPointerDown(data);
+                        }
+                    }
+                    else
+                    {
+                        if (mActiveTouches.Contains(data.InputId))
+                        {
+                            mActiveTouches.Remove(data.InputId);
+                            if (mActiveTouches.Count == 0)
+                                mActiveDevice = null;
+                            //POINTER UP
+                            OnRawPointerUp(data);
+                        }
+                    }
+                }
+
+                var isMouse = context.control.device is Mouse;
+                var isPen = context.control.device is Pen;
+
+                if (isMouse || isPen)
+                {
+                    //We do not have ID in the beginning. So we fill it here
+                    var id = isMouse ? Helpers.LeftMouseInputId : Helpers.PenInputId;
+                    data.InputId = id;
+
+                    if (data.Contact)
+                    {
+                        if (mIsMouseOrPenInputStarted)
+                        {
+                            //POINTER DRAG
+                            OnRawPointerDrag(data);
+                        }
+                        else
+                        {
+                            mIsMouseOrPenInputStarted = true;
+                            mActiveDevice = context.control.device;
+                            //POINTER DOWN
+                            OnRawPointerDown(data);
+                        }
+                    }
+                    else
+                    {
+                        if (mIsMouseOrPenInputStarted)
+                        {
+                            mIsMouseOrPenInputStarted = false;
+                            mActiveDevice = null;
+                            //POINTER UP
+                            OnRawPointerUp(data);
+                        }
+                    }
+                }
+            }
+            else
+            {
+                Debug.Log("Cant be handled");
+            }
+        }
+
+        private List<Transform> mTestTransforms = new List<Transform>();
+        private Plane mTestPlane = new Plane(Vector3.back, Vector3.zero);
+
+        private void OnRawPointerDown(PointerInput data)
+        {
+            Debug.Log($"TestPointerDown {data.InputId} {data.Position}");
+            mActivePointers.Add(data.InputId, new InteractionPointerData(data.Position, data.InputId, mActivePointers.Count == 0));
+
+            var t = GameObject.CreatePrimitive(PrimitiveType.Sphere).transform;
+            mTestTransforms.Add(t);
+            Set2dPos(data.Position, t);
+        }
+
+        private void OnRawPointerUp(PointerInput data)
+        {
+            Debug.Log($"TestPointerUp {data.InputId}, {data.Position}");
+            mActivePointers.Remove(data.InputId);
+            var last = mTestTransforms[mTestTransforms.Count - 1];
+            mTestTransforms.RemoveAt(mTestTransforms.Count - 1);
+            Destroy(last.gameObject);
+
+            var counter = 0;
+            foreach (var activePointer in mActivePointers.Values)
+                Set2dPos(activePointer.PointerPosition, mTestTransforms[counter++]);
+        }
+
+        private void OnRawPointerDrag(PointerInput data)
+        {
+            Debug.Log($"TestPointerMove {data.InputId}, {data.Position}");
+            mActivePointers[data.InputId].UpdatePosition(data.Position);
+
+            var counter = 0;
+            foreach (var activePointer in mActivePointers.Values)
+                Set2dPos(activePointer.PointerPosition, mTestTransforms[counter++]);
+        }
+
+        private void Set2dPos(Vector2 pos, Transform t)
+        {
+            var ray = mCameras[0].ScreenPointToRay(pos);
+            mTestPlane.Raycast(ray, out var enter);
+            var point3d = ray.GetPoint(enter);
+            t.transform.position = point3d;
+        }
+
+        /// <summary>
+        /// Checks if action can be handled from this device. We do not allow to use pen/mouse and touch at the same time
+        /// </summary>
+        /// <param name="context"></param>
+        /// <returns></returns>
+        private bool CanBeHandled(InputAction.CallbackContext context)
+        {
+            return mActiveDevice == null || mActiveDevice.GetType() == context.control.device.GetType();
         }
 
         /// <summary>
@@ -62,9 +221,9 @@ namespace ViJTools
         /// </summary>
         private void OnDisable()
         {
-            mActions.GestureActions.PointerStart.performed -= OnPointerPerformed;
-            mActions.GestureActions.PointerStart.canceled -= OnPointerCanceled;
-            mActions.GestureActions.PointerMove.performed -= OnPointerMove;
+            //mActions.GestureActions.PointerStart.performed -= OnPointerPerformed;
+            //mActions.GestureActions.PointerStart.canceled -= OnPointerCanceled;
+            //mActions.GestureActions.PointerMove.performed -= OnPointerMove;
             mActions.Dispose();
         }
 
